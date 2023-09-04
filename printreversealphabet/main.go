059fd8fe91c10772295ab921c3bf3d66fdd3d900
hadhaif <hassanali61911@gmail.com>
2023-09-04 15:05:08 +0300

    second go program

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func main() {
 	alpha := "abcdefghijklmnopqrstuvwxyz"
 
-	for i := len(alpha) - 1; i > 0; i-- {
+	for i := len(alpha) - 1; i >= 0; i-- {
 		z01.PrintRune(rune(alpha[i]))
 	}
 	z01.PrintRune('\n')
