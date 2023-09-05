8ef827b8247011edf2c9fc757fad09a617ff4ccd
hasanali117 <hassanali61911@gmail.com>
2023-09-05 16:55:26 +0300

    updated

--- test/main.go
+++ test/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	a := 0
-	b := &a
-	n := &b
-	piscine.UltimatePointOne(&n)
-	fmt.Println(a)
+	a := 13
+	b := 2
+	var div int
+	var mod int
+	piscine.DivMod(a, b, &div, &mod)
+	fmt.Println(div)
+	fmt.Println(mod)
 }
