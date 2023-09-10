7f3d97bdfb8e47b576f4bb914419a0646f4e1ea9
hasanali117 <hassanali61911@gmail.com>
2023-09-10 18:06:14 +0300

    updated

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -3,13 +3,13 @@ package piscine
 func IterativeFactorial(nb int) int {
 	if nb == 1 {
 		return 1
-	}
-	if nb > 1 {
+	} else if nb > 1 {
 		num := 1
 		for i := nb; i > 1; i-- {
 			num *= i
 		}
 		return num
+	} else {
+		return 0
 	}
-	return 0
 }
