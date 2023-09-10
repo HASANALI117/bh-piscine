7bc95e4b244998674db3c1b6ed6a96c1d005e12b
hasanali117 <hassanali61911@gmail.com>
2023-09-10 17:47:04 +0300

    updated

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,10 +4,10 @@ func IterativeFactorial(nb int) int {
 	if nb == 1 {
 		return 1
 	}
-	num := 1
 	if nb > 1 {
+		num := 1
 		for i := 1; i <= nb; i++ {
-			num = num * i
+			num *= i
 		}
 		return num
 	}
