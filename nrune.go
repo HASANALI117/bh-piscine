d87b00dad751d5d3ffe67022c7f7e25d484c4653
hasanali117 <hassanali61911@gmail.com>
2023-09-11 18:27:35 +0300

    updated

--- nrune.go
+++ nrune.go
@@ -2,8 +2,8 @@ package piscine
 
 func NRune(s string, n int) rune {
 	run := []rune(s)
-	if n < len(run) && n >= 0 {
-		return run[n]
+	if n <= len(run) && n >= 0 {
+		return run[n-1]
 	}
 	return 0
 }
