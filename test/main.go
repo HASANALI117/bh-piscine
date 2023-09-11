af80cab68a93d3112073aaada224d322d48bcc3a
hasanali117 <hassanali61911@gmail.com>
2023-09-11 18:09:27 +0300

    updated

--- test/main.go
+++ test/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	z01.PrintRune(piscine.FirstRune("Hello!"))
-	z01.PrintRune(piscine.FirstRune("Salut!"))
-	z01.PrintRune(piscine.FirstRune("Ola!"))
+	z01.PrintRune(piscine.LastRune("Hello!"))
+	z01.PrintRune(piscine.LastRune("Salut!"))
+	z01.PrintRune(piscine.LastRune("Ola!"))
 	z01.PrintRune('\n')
 }
