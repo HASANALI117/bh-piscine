b7ac9b9a314d5485d8c3c141a5e58a7f471a6df5
hasanali117 <hassanali61911@gmail.com>
2023-09-20 01:32:26 +0300

    updated

--- point/main.go
+++ point/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"github.com/01-edu/z01"
+)
+
 type point struct {
 	x int
 	y int
@@ -8,4 +12,24 @@ type point struct {
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
+}
+
+func print(s string) {
+	for _, char := range s {
+		z01.PrintRune(char)
+	}
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	print("x = ")
+	z01.PrintRune(rune(points.x/10 + 48))
+	z01.PrintRune(rune(points.x%10 + 48))
+
+	print(", y = ")
+	z01.PrintRune(rune(points.y/10 + 48))
+	z01.PrintRune(rune(points.y%10 + 48))
 }
