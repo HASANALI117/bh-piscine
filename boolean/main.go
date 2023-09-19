a20d693126788058b77427f342e36de30b949a12
hasanali117 <hassanali61911@gmail.com>
2023-09-19 17:30:24 +0300

    updated

---
+++ boolean/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(s string) {
+	for _, r := range s {
+		z01.PrintRune(r)
+	}
+	z01.PrintRune('\n')
+}
+
+func isEven(nbr int) bool {
+	if nbr % 2 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	if isEven(len(os.Args)) {
+		printStr("even")
+	} else {
+		printStr("odd")
+	}
+}
