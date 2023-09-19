91e06390a9fb2d196d41135861f29bac61280e05
hasanali117 <hassanali61911@gmail.com>
2023-09-19 19:37:53 +0300

    updated

--- point/main.go
+++ point/main.go
@@ -1,7 +1,5 @@
 package main
 
-import "fmt"
-
 type point struct {
 	x int
 	y int
@@ -10,12 +8,4 @@ type point struct {
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
-}
-
-func main() {
-	points := &point{}
-
-	setPoint(points)
-
-	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
 }
