875fe83157d2bcba5786a1e402f80ba6f8b03799
hasanali117 <hassanali61911@gmail.com>
2023-09-21 11:34:59 +0300

    updated

--- printcomb.go
+++ printcomb.go
@@ -3,17 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0; i < 10; i++ {
-		for j := i + 1; j < 10; j++ {
-			for k := j + 1; k < 10; k++ {
-				if i == 7 {
-					z01.PrintRune(rune(i) + 48)
-					z01.PrintRune(rune(j) + 48)
-					z01.PrintRune(rune(k) + 48)
+	for i := '0'; i <= '9'; i++ {
+		for j := i + 1; j <= '9'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				if i == '7' {
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 				} else {
-					z01.PrintRune(rune(i) + 48)
-					z01.PrintRune(rune(j) + 48)
-					z01.PrintRune(rune(k) + 48)
+					z01.PrintRune(i)
+					z01.PrintRune(j)
+					z01.PrintRune(k)
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
