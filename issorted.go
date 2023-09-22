56572165a3018e27c0a33bdd40e00f6eb73e7a8f
hasanali117 <hassanali61911@gmail.com>
2023-09-22 19:24:12 +0300

    updated

--- issorted.go
+++ issorted.go
@@ -1,12 +1,16 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
+	ascending := true
+	descending := true
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) >= 0 {
-			return false
+		if f(a[i], a[i+1]) > 0 {
+			ascending = false
+		} else if f(a[i], a[i+1]) < 0 {
+			descending = false
 		}
 	}
-	return true
+	return ascending || descending
 }
 
 func IsSorted2(a, b int) int {
