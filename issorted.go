ef2f0a111659dab6af4d500632b1b87fac0b5eac
hasanali117 <hassanali61911@gmail.com>
2023-09-22 17:17:20 +0300

    updated

---
+++ issorted.go
@@ -0,0 +1,20 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for i := 0; i < len(a)-1; i++ {
+		if f(a[i], a[i+1]) != -1 {
+			return false
+		}
+	}
+	return true
+}
+
+func IsSorted2(a, b int) int {
+	if a > b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
+}
--- test/main.go
+++ test/main.go
@@ -6,10 +6,12 @@ import (
 )
 
 func main() {
-	tab1 := []string{"Hello", "how", "are", "you"}
-	tab2 := []string{"This", "1", "is", "4", "you"}
-	answer1 := piscine.CountIf(piscine.IsNumeric, tab1)
-	answer2 := piscine.CountIf(piscine.IsNumeric, tab2)
-	fmt.Println(answer1)
-	fmt.Println(answer2)
+	a1 := []int{0, 1, 2, 3, 4, 5}
+	a2 := []int{0, 2, 1, 3}
+
+	result1 := piscine.IsSorted(piscine.IsSorted2, a1)
+	result2 := piscine.IsSorted(piscine.IsSorted2, a2)
+
+	fmt.Println(result1)
+	fmt.Println(result2)
 }
