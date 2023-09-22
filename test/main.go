24a2bcba7f008fdb655d097857462a54535ed9c0
hasanali117 <hassanali61911@gmail.com>
2023-09-22 17:27:11 +0300

    updated

--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{-995198, -933815, -600589, -527392, 47076, 165895, 280637, 283022}
+	a2 := []int{-957199, -715862, -172884, -84325, -50793, 152869, 209515, 481400}
 
 	result1 := piscine.IsSorted(piscine.IsSorted2, a1)
 	result2 := piscine.IsSorted(piscine.IsSorted2, a2)
