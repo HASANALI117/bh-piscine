b7a9b6c7ab68391e77820cf4c7aa6ea63c599b56
hasanali117 <hassanali61911@gmail.com>
2023-09-22 15:49:02 +0300

    updated

--- foreach.go
+++ foreach.go
@@ -1,15 +1,7 @@
 package piscine
 
-import (
-	"github.com/01-edu/z01"
-)
-
 func ForEach(f func(int), a []int) {
 	for _, num := range a {
 		f(num)
 	}
-}
-
-func PrintNbr(i int) {
-	z01.PrintRune(rune(i) + 48)
 }
---
+++ printnbr.go
@@ -0,0 +1,9 @@
+package piscine
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func PrintNbr(i int) {
+	z01.PrintRune(rune(i) + 48)
+}
