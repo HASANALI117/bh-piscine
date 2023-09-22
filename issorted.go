f9bf2ba16612b74b6bd6d44aa7d582d81515ecef
hasanali117 <hassanali61911@gmail.com>
2023-09-22 19:14:51 +0300

    updated

--- issorted.go
+++ issorted.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := 0; i < len(a)-1; i++ {
-		if f(a[i], a[i+1]) != -1 {
+		if f(a[i], a[i+1]) >= 0 {
 			return false
 		}
 	}
--- test/main.go
+++ test/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	a1 := []int{0, 1, 2, 3, 4, 5}
-	a2 := []int{0, 2, 1, 3, 4}
+	a2 := []int{-983690, -808955, -487942, -364693, 244344, 273682, 588124, 780714}
 
 	result1 := piscine.IsSorted(piscine.IsSorted2, a1)
 	result2 := piscine.IsSorted(piscine.IsSorted2, a2)
